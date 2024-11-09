bf92a8a956ad17ddefa492049ecfb3742bb03696
prosg <learner.in.dev@gmail.com>
2024-11-09 15:44:24 +0100

    Ajout du composant Header et Footer JSX

---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+const navigation = [
+  {
+    name: "LinkedIn",
+    href: "#",
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path d="M22.23 0H1.77A1.77 1.77 0 000 1.77v20.46A1.77 1.77 0 001.77 24h20.46A1.77 1.77 0 0024 22.23V1.77A1.77 1.77 0 0022.23 0zM7.09 20.16H3.56V9h3.53v11.16zm-1.76-12.8a2.05 2.05 0 11.01-4.1 2.05 2.05 0 01-.01 4.1zm15.27 12.8h-3.53v-5.73c0-1.37-.03-3.13-1.91-3.13-1.91 0-2.2 1.49-2.2 3.03v5.83h-3.53V9h3.39v1.52h.05a3.72 3.72 0 013.34-1.83c3.57 0 4.23 2.35 4.23 5.42v6.05z" />
+      </svg>
+    ),
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/JulieDG9",
+    icon: (props) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900">
+      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center gap-x-6 md:order-2">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-400 hover:text-gray-300"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon aria-hidden="true" className="h-6 w-6" />
+            </a>
+          ))}
+        </div>
+        <p className="mt-8 text-center text-sm/6 text-gray-400 md:order-1 md:mt-0">
+          &copy; 2024 Julie Dumond - Développeuse Front-End, Tous droits
+          réservés.
+        </p>
+      </div>
+    </footer>
+  );
+}
